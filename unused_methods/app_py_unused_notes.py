d543349d7160ebd04e8549296855c8d549223bfa
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 10:43:58 -0700

    updated copyright format

--- unused_methods/app_py_unused_notes.py
+++ unused_methods/app_py_unused_notes.py
@@ -1,3 +1,9 @@
+
+# Copyright (c) 2018 Michael Neill Hartman. All rights reserved.
+# mnh_license@proton.me
+# https://github.com/hartmanm
+# oros / overlord (previously openrig.net)
+
 app.py unused notes
 
 
