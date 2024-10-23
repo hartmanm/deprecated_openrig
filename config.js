cd05644a9574485e0333fc78bea3b6085d8b08ed
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 15:30:25 -0700

    initial config

---
+++ config.js
@@ -0,0 +1,45 @@
+
+const img_css_style         ='style="width:50px;height:50px;"'
+
+const static_src_url        ='https://hartmanm.github.io/static/overlord/'
+
+const static_w10_src_url    ='https://hartmanm.github.io/static/overlord/win10guide/'
+
+const img_row_1_a  ='<img src="'+static_src_url+'d3_road_deaths.png" '+img_css_style+'/>'
+
+// images
+const eth_img       =''+static_src_url+'ether.png'
+const etc_img       =''+static_src_url+'etherclassic.png'
+const rvn_img       =''+static_src_url+'rvn.png'
+const xmr_img       =''+static_src_url+'xmr.png'
+
+const w10_src_1     =''+static_w10_src_url+'w1.png'
+const w10_src_2     =''+static_w10_src_url+'w2.png'
+const w10_src_3     =''+static_w10_src_url+'w3.png'
+const w10_src_4     =''+static_w10_src_url+'w4.png'
+const w10_src_5     =''+static_w10_src_url+'w5.png'
+const w10_src_6     =''+static_w10_src_url+'w6.png'
+const w10_src_7     =''+static_w10_src_url+'w7.png'
+const w10_src_8     =''+static_w10_src_url+'w8.png'
+const w10_src_9     =''+static_w10_src_url+'w9.png'
+const w10_src_10    =''+static_w10_src_url+'w10.png'
+const w10_src_11    =''+static_w10_src_url+'w11.png'
+const w10_src_12    =''+static_w10_src_url+'w12.png'
+const w10_src_13    =''+static_w10_src_url+'w13.png'
+const w10_src_14    =''+static_w10_src_url+'w14.png'
+const w10_src_15    =''+static_w10_src_url+'w15.png'
+const w10_src_16    =''+static_w10_src_url+'w16.png'
+const w10_src_17    =''+static_w10_src_url+'w17.png'
+const w10_src_18    =''+static_w10_src_url+'w18.png'
+const w10_src_19    =''+static_w10_src_url+'w19.png'
+const w10_src_20    =''+static_w10_src_url+'w20.png'
+const w10_src_21    =''+static_w10_src_url+'w21.png'
+const w10_src_22    =''+static_w10_src_url+'w22.png'
+const w10_src_23    =''+static_w10_src_url+'w23.png'
+const w10_src_24    =''+static_w10_src_url+'w24.png'
+const w10_src_25    =''+static_w10_src_url+'w25.png'
+const w10_src_26    =''+static_w10_src_url+'w26.png'
+const w10_src_27    =''+static_w10_src_url+'w27.png'
+const w10_src_28    =''+static_w10_src_url+'w28.png'
+
+
