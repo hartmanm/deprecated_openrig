5bd18f648323081c745c2b25e8dba322a6a1bcff
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 10:44:40 -0700

    update copyright format

--- unused_methods/edit_rig_put.js
+++ unused_methods/edit_rig_put.js
@@ -1,3 +1,10 @@
+/*
+# Copyright (c) 2018 Michael Neill Hartman. All rights reserved.
+# mnh_license@proton.me
+# https://github.com/hartmanm
+# oros / overlord (previously openrig.net)
+*/
+
 //editrig PUT
 
 
